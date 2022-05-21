f80c37e6ac5a3f5384b26e403fadeb1fbf0de78f
DevTobias <tobi.kaerst@gmx.de>
2022-05-21 16:36:46 +0200

    feat: added ability for adornment

--- components/text-field/src/TextField/TextField.tsx
+++ components/text-field/src/TextField/TextField.tsx
@@ -60,10 +60,27 @@ export const TextField: FunctionComponent<TextFieldProps> = ({
     ...((maxLength ?? 0) > 0 && { maxLength }),
   };
 
+  const eye = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      className={styles.test}
+      viewBox="0 0 20 20"
+      fill="currentColor"
+    >
+      <path d="M10 12a2 2 0 100-4 2 2 0 000 4z" />
+      <path
+        fillRule="evenodd"
+        d="M.458 10C1.732 5.943 5.522 3 10 3s8.268 2.943 9.542 7c-1.274 4.057-5.064 7-9.542 7S1.732 14.057.458 10zM14 10a4 4 0 11-8 0 4 4 0 018 0z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+
   return (
     <div className={containerClasses}>
       <div className={textFieldClasses}>
         <input id={textFieldId} {...inputProps} />
+        <div></div>
 
         <Show when={label}>
           <label htmlFor={textFieldId}>{label}</label>
@@ -73,6 +90,8 @@ export const TextField: FunctionComponent<TextFieldProps> = ({
             </fieldset>
           </Show>
         </Show>
+
+        {eye}
       </div>
 
       <div className={styles.below}>
