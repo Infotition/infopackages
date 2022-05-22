66ff6fad9458ecb96b62074d8fff11330a392564
DevTobias <tobi.kaerst@gmx.de>
2022-05-22 22:39:45 +0200

    feat: renamed components to react

--- components/text-field/__tests__/index.test.tsx
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-
-import { Button } from '../src';
-
-it('should render button', () => {
-  render(<Button />);
-  const btn = screen.getByTestId('btn');
-  expect(btn).toHaveTextContent('Hello World');
-});
